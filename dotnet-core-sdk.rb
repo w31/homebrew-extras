b294ba57f67a4bcc4a12ae83003471c2d08347ee
Wei <w31@users.noreply.github.com>
2017-03-08 22:10:31 +1100

    .NET Core 1.1.1

--- dotnet-core-sdk.rb
+++ dotnet-core-sdk.rb
@@ -1,16 +1,16 @@
 class DotnetCoreSdk < Formula
   desc ".NET Core SDK"
   homepage "http://www.microsoft.com/net/core"
-  url "http://download.microsoft.com/download/A/F/6/AF610E6A-1D2D-47D8-80B8-F178951A0C72/Binaries/dotnet-dev-osx-x64.1.0.0-preview2-1-003177.tar.gz"
-  version "1.1"
-  sha256 "8f1ffef37aa9da7273674185dab3b78feb44082d3a9d3156dfc20329acfe7e0d"
+  url "https://download.microsoft.com/download/F/D/5/FD52A2F7-65B6-4912-AEDD-4015DF6D8D22/dotnet-1.1.1-sdk-osx-x64.tar.gz"
+  version "1.1.1"
+  sha256 "f030188673ebb71fc3bb3a089504c2079cd8176e669575b5992664b405194366"
 
   depends_on "openssl"
 
   def install
     libexec.install Dir["*"]
-    (libexec + "shared/Microsoft.NETCore.App/1.1.0/libcrypto.1.0.0.dylib").make_symlink Formula["openssl"].lib/"libcrypto.1.0.0.dylib"
-    (libexec + "shared/Microsoft.NETCore.App/1.1.0/libssl.1.0.0.dylib").make_symlink Formula["openssl"].lib/"libssl.1.0.0.dylib"
+    (libexec + "shared/Microsoft.NETCore.App/1.1.1/libcrypto.1.0.0.dylib").make_symlink Formula["openssl"].lib/"libcrypto.1.0.0.dylib"
+    (libexec + "shared/Microsoft.NETCore.App/1.1.1/libssl.1.0.0.dylib").make_symlink Formula["openssl"].lib/"libssl.1.0.0.dylib"
     bin.write_exec_script libexec/"dotnet"
   end
 
