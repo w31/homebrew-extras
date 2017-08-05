7864794e5bbadd6dbf900b6accc2cfdedfd59665
Wei <w31@users.noreply.github.com>
2017-08-05 12:29:08 +1000

    Update dotnet-core-sdk to 2.0 Preview 2

--- dotnet-core-sdk.rb
+++ dotnet-core-sdk.rb
@@ -1,16 +1,12 @@
 class DotnetCoreSdk < Formula
   desc ".NET Core SDK"
   homepage "http://www.microsoft.com/net/core"
-  url "https://download.microsoft.com/download/F/D/5/FD52A2F7-65B6-4912-AEDD-4015DF6D8D22/dotnet-1.1.1-sdk-osx-x64.tar.gz"
-  version "1.1.1"
-  sha256 "f030188673ebb71fc3bb3a089504c2079cd8176e669575b5992664b405194366"
-
-  depends_on "openssl"
+  url "https://download.microsoft.com/download/F/A/A/FAAE9280-F410-458E-8819-279C5A68EDCF/dotnet-sdk-2.0.0-preview2-006497-osx-x64.tar.gz"
+  version "2.0.0-preview2"
+  sha256 "97dfd57b1e3f3c6ae365654141d3670619699449cca76c3d7ef13a9f0b967ad5"
 
   def install
     libexec.install Dir["*"]
-    (libexec + "shared/Microsoft.NETCore.App/1.1.1/libcrypto.1.0.0.dylib").make_symlink Formula["openssl"].lib/"libcrypto.1.0.0.dylib"
-    (libexec + "shared/Microsoft.NETCore.App/1.1.1/libssl.1.0.0.dylib").make_symlink Formula["openssl"].lib/"libssl.1.0.0.dylib"
     bin.write_exec_script libexec/"dotnet"
   end
 
