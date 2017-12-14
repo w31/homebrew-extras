a78063c9d9d74905b772f0b430489e188e5f976d
Wei <w31@users.noreply.github.com>
2017-12-14 22:58:38 +1100

    Update dotnet-core-sdk to 2.1.2.

--- dotnet-core-sdk.rb
+++ dotnet-core-sdk.rb
@@ -1,9 +1,9 @@
 class DotnetCoreSdk < Formula
   desc ".NET Core SDK"
   homepage "http://www.microsoft.com/net/core"
-  url "https://download.microsoft.com/download/D/7/2/D725E47F-A4F1-4285-8935-A91AE2FCC06A/dotnet-sdk-2.0.3-osx-x64.tar.gz"
-  version "2.0.3"
-  sha256 "6f415f6caada73f14b3ee706054a7d9b40087cc794e815c7fe74ce7afa8bef2d"
+  url "https://download.microsoft.com/download/5/D/F/5DF4B836-7DFD-4CCF-AC96-101E2A4C7421/dotnet-sdk-2.1.2-osx-x64.tar.gz"
+  version "2.1.2"
+  sha256 "fa660e419add9df6a5a2cbbcfc622a694aa7de0986a46322b744ac138172b264"
 
   def install
     libexec.install Dir["*"]
