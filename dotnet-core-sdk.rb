c7162cc93eb2fb0f33d5a89bb61f690249a13992
Wei <w31@users.noreply.github.com>
2020-08-13 19:58:45 +1000

    dotnet-core-sdk 3.1.401

--- dotnet-core-sdk.rb
+++ dotnet-core-sdk.rb
@@ -1,9 +1,9 @@
 class DotnetCoreSdk < Formula
   desc ".NET Core SDK"
   homepage "http://www.microsoft.com/net/core"
-  url "https://download.visualstudio.microsoft.com/download/pr/271e75d7-5d38-4e7c-978f-6871bed679c8/fab96db72433839ad0353c6dec2692e3/dotnet-sdk-3.1.302-osx-x64.tar.gz"
-  version "3.1.302"
-  sha256 "66378cdab218ffe1684dddeb82d7ff9278abf1104a52dd67d87bb3d6ce6c8ff8"
+  url "https://download.visualstudio.microsoft.com/download/pr/e1f6e8dc-833f-46aa-866b-40b9bc68ed0f/6540a60960a6489061a08a9ccd3935cd/dotnet-sdk-3.1.401-osx-x64.tar.gz"
+  version "3.1.401"
+  sha256 "57c8ebccc581175a1716ce9ca42d040760a3f8c1255a1ee4b844ecf454717044"
 
   def install
     (share/"dotnet").install Dir["*"]
