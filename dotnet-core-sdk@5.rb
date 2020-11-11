df11b31b807443fd8389176ba98285ea5712cabc
Wei <w31@users.noreply.github.com>
2020-11-11 21:18:29 +1100

    dotnet-core-sdk@5 5.0.100

--- dotnet-core-sdk@5.rb
+++ dotnet-core-sdk@5.rb
@@ -1,9 +1,9 @@
 class DotnetCoreSdkAT5 < Formula
   desc ".NET Core SDK"
   homepage "http://www.microsoft.com/net/core"
-  url "https://download.visualstudio.microsoft.com/download/pr/c0cafea0-6f76-407a-8c70-175c25e701d8/de9cc8367c469fa7eaaf5a7fb4aaf08d/dotnet-sdk-5.0.100-rc.2.20479.15-osx-x64.tar.gz"
-  version "5.0.100-rc2"
-  sha256 "ec848f8f83c5ec88f4d3bad061121569bffa484cae9046b1d9a44a88d243b421"
+  url "https://download.visualstudio.microsoft.com/download/pr/0871336f-9a83-4ce4-80ca-625d03003369/2eb78456e0b106e9515dc03898d3867a/dotnet-sdk-5.0.100-osx-x64.tar.gz"
+  version "5.0.100"
+  sha256 "cbe223333a9724e83bd1448db30a8b5136a46a3d1c1a1f42c26e31b01523a389"
 
   def install
     (share/"dotnet").install Dir["*"]
