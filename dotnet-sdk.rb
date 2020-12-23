8e2423f1bd07eeb466ed8472629003bcc03bc459
Wei <w31@users.noreply.github.com>
2020-12-24 10:34:23 +1100

    dotnet-sdk: renamed from dotnet-core-sdk@5.rb

--- dotnet-sdk.rb
+++ dotnet-sdk.rb
@@ -1,6 +1,6 @@
-class DotnetCoreSdkAT5 < Formula
-  desc ".NET Core SDK"
-  homepage "http://www.microsoft.com/net/core"
+class DotnetSdk < Formula
+  desc ".NET SDK"
+  homepage "https://dotnet.microsoft.com/"
   url "https://download.visualstudio.microsoft.com/download/pr/1193fc39-e498-407d-bf65-071172fbfb8f/56c95047d1d187cec6dd107674b650a8/dotnet-sdk-5.0.101-osx-x64.tar.gz"
   version "5.0.101"
   sha256 "b8a2628d31ff7d68823b63ea352189d1e91a358ecd62739cb8c2bddf2e02763f"
