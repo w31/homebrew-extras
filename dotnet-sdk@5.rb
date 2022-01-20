4022937099b84552de05f87e27da2d2d2097acf1
Wei <w31@users.noreply.github.com>
2022-01-21 09:57:19 +1100

    dotnet-sdk@5 5.0.403 -> 5.0.404

--- dotnet-sdk@5.rb
+++ dotnet-sdk@5.rb
@@ -1,9 +1,9 @@
 class DotnetSdkAT5 < Formula
   desc ".NET SDK"
   homepage "https://dotnet.microsoft.com/"
-  url "https://download.visualstudio.microsoft.com/download/pr/5ccdb916-531f-4064-84e8-5475b273a4de/80dcfa0c2eb528f8b0e7c313ed36f4f1/dotnet-sdk-5.0.403-osx-x64.tar.gz"
-  version "5.0.403"
-  sha256 "a3d7bd454882ab97d6de7278adbea08e4a13af2501a3ababa5ef3fb71ef278ce"
+  url "https://download.visualstudio.microsoft.com/download/pr/90588609-df30-4cb7-b4aa-a2e71ec42c9a/9bc894713f459ebe73493552fd231807/dotnet-sdk-5.0.404-osx-x64.tar.gz"
+  version "5.0.404"
+  sha256 "56c2b1515ec22cd7ed41f868859cba0043af0f82506f90f9e9ad226ed6b15d4e"
 
   def install
     (share/"dotnet").install Dir["*"]
