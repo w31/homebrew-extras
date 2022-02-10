f720d2677130c06ad1b4d8eac5e1fbbe6291f2b6
Wei <w31@users.noreply.github.com>
2022-02-10 21:41:03 +1100

    dotnet-sdk 6.0.100 -> 6.0.102

--- dotnet-sdk.rb
+++ dotnet-sdk.rb
@@ -1,9 +1,9 @@
 class DotnetSdk < Formula
   desc ".NET SDK 6.0"
   homepage "https://dotnet.microsoft.com/"
-  url "https://download.visualstudio.microsoft.com/download/pr/62f78047-71de-460e-85ca-254f1fa848de/ecabeefdca2902f3f06819612cd9d45c/dotnet-sdk-6.0.100-osx-x64.tar.gz"
-  version "6.0.100"
-  sha256 "6938e387b3e0c059683ef6ffb4c3ef726189ea88134462666cd6e346ad41294a"
+  url "https://download.visualstudio.microsoft.com/download/pr/17115de2-6099-4c24-9a66-d89d25f52968/9f55035d1a43a5d133c6a0bd35230d69/dotnet-sdk-6.0.102-osx-x64.tar.gz"
+  version "6.0.102"
+  sha256 "caec2b4d4f28b36fde1185c76a6783070360ca0526da90148c076dc4947d9ae6"
 
   def install
     (share/"dotnet").install Dir["*"]
