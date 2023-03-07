85325739d2f4683863681db7aa56ad4bc8a0bc3a
Wei <w31@users.noreply.github.com>
2023-03-07 23:24:15 +1100

    dotnet-sdk@6: renamed from dotnet-sdk

--- dotnet-sdk@6.rb
+++ dotnet-sdk@6.rb
@@ -1,4 +1,4 @@
-class DotnetSdk < Formula
+class DotnetSdkAT6 < Formula
   desc ".NET SDK 6.0"
   homepage "https://dotnet.microsoft.com/"
   url "https://download.visualstudio.microsoft.com/download/pr/43189462-e807-4a01-88fe-d348d72130ad/29abc60cb75e8bcb12df60c2930fec25/dotnet-sdk-6.0.404-osx-x64.tar.gz"
