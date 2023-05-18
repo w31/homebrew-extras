b67788ad828ded59680253e82c26e8a29d9f398f
Wei <w31@users.noreply.github.com>
2023-05-18 20:22:49 +1000

    dotnet-sdk 7.0.202 -> 7.0.302

--- dotnet-sdk.rb
+++ dotnet-sdk.rb
@@ -5,12 +5,12 @@ class DotnetSdk < Formula
 
   on_macos do
     on_arm do
-      url "https://download.visualstudio.microsoft.com/download/pr/4402413a-ef81-4732-a0c0-38f11694b226/e205b8bf48d95902a6dbe1c3cccca272/dotnet-sdk-7.0.202-osx-arm64.tar.gz"
-      sha256 "7f8201d42d31d6985288d0e31a32aa3b1dff40fceb8cb202993c0b72883bcebb"
+      url "https://download.visualstudio.microsoft.com/download/pr/fc7ed56d-3afe-4aa6-81bb-b4b0f5df56b5/d199f43f7421c6677ba25544b442b6b7/dotnet-sdk-7.0.302-osx-arm64.tar.gz"
+      sha256 "460f835ef19c7655384e88f7eb457b2aeb92b8723a692f0ab00232f5b66315f8"
     end
     on_intel do
-      url "https://download.visualstudio.microsoft.com/download/pr/d3fe9043-0ff4-4584-aacf-1ad41c47407b/7b84ed341359488cd0de21de1b4df6d0/dotnet-sdk-7.0.202-osx-x64.tar.gz"
-      sha256 "1531bfa7bf384f8b5c433d87af170f7839b130ea9c20b617aaf41742a13eefa9"
+      url "https://download.visualstudio.microsoft.com/download/pr/34ce4803-1444-48a2-9955-e2a9b9061b03/e18c978b55226240ca037cf8b1770064/dotnet-sdk-7.0.302-osx-x64.tar.gz"
+      sha256 "a49ce1d08083f062b500651fd765905dbcdd8635eb0c9b57a671e79f18136ca0"
     end
   end
 
