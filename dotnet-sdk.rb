869f86c72d2a4edcf460ce45ba697177199e4509
Wei <w31@users.noreply.github.com>
2024-09-28 21:41:40 +1000

    dotnet-sdk 8.0.302 -> 8.0.402

--- dotnet-sdk.rb
+++ dotnet-sdk.rb
@@ -1,16 +1,16 @@
 class DotnetSdk < Formula
   desc ".NET SDK 8.0"
   homepage "https://dotnet.microsoft.com/"
-  version "8.0.302"
+  version "8.0.402"
 
   on_macos do
     on_arm do
-      url "https://download.visualstudio.microsoft.com/download/pr/9d5ec61f-58b3-412f-a4b7-be8c295b4877/fcd77a3d07f2c2054b86154634402527/dotnet-sdk-8.0.302-osx-arm64.tar.gz"
-      sha256 "0a786792c6ff41a7cf3c5d43bc2bbffe4a96a9c9df709cb816111ff670d33eb9"
+      url "https://download.visualstudio.microsoft.com/download/pr/02e8cab1-b111-46c5-be2c-7cf4faa3eb0f/bdb1b083203768de0a59d4f1c4d8abbb/dotnet-sdk-8.0.402-osx-arm64.tar.gz"
+      sha256 "ddeff38e1c5e0985b5a2ee79ad1e7477861cfcc01c407704fb11cfbda180a968"
     end
     on_intel do
-      url "https://download.visualstudio.microsoft.com/download/pr/8893b99f-aca2-4f93-af7b-cf6017cf5f7b/e45804f1d91b9b01ebd5b15a29e9c088/dotnet-sdk-8.0.302-osx-x64.tar.gz"
-      sha256 "183d575fe4604cac142145204f202857ceaf6dcda5a21e7d04df2b719d44d761"
+      url "https://download.visualstudio.microsoft.com/download/pr/351ba3c9-191f-4382-bd69-046622fbb656/283df9aa63ec1392843da7a0f473cc9c/dotnet-sdk-8.0.402-osx-x64.tar.gz"
+      sha256 "e161131bd1870e7855fdb286b1b20f8d25005042e0d24b8014b14ed9087b3aa6"
     end
   end
 
